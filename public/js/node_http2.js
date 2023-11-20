ca6ca78d3efd335c4b82b49dd1eb10391b85bc2d
allanmuraroto <allan.muraroto@hotmail.com>
2023-11-19 23:18:46 -0300

    first commit

---
+++ public/js/node_http2.js
@@ -0,0 +1,13 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["node_http2"],{
+
+/***/ "node:http2":
+/*!******************!*\
+  !*** node:http2 ***!
+  \******************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:http2\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\NormalModule.js:838:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\NormalModule.js:835:8)\n    at processResource (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\loader-runner\\lib\\LoaderRunner.js:398:2)\n    at NormalModule._doBuild (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\NormalModule.js:825:3)\n    at NormalModule.build (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\NormalModule.js:969:15)\n    at C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\Compilation.js:1377:12\n    at NormalModule.needBuild (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\NormalModule.js:1257:32)\n    at Compilation._buildModule (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\Compilation.js:1358:10)\n    at C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\util\\AsyncQueue.js:305:10\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at AsyncQueue._startProcessing (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\util\\AsyncQueue.js:295:26)\n    at AsyncQueue._ensureProcessing (C:\\Users\\Allan\\Documents\\Projetos\\Laravel\\assinatura\\node_modules\\webpack\\lib\\util\\AsyncQueue.js:282:12)\n    at process.processImmediate (node:internal/timers:471:21)");
+
+/***/ })
+
+}]);
